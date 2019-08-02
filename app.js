9d4a40b242c1a8b968d68680a2ef865b0f4846a8
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-02 03:23:58 +0300

    fixed require

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const config = require('config');
+const serverDebugger = require("debug")("app:server");
+const config = require("config");
+const express = require("express");
 
-const port = process.env.PORT || process.argv[2] || config.get('port') || 8000;
+const port = process.env.PORT || process.argv[2] || config.get("port") || 8000;
 
-const app = require('./startup/server');
+const app = express();
 
-require('./startup/logger')(app);
-require('./startup/database')();
-require('./startup/config')(app);
-require('./startup/middleware')(app);
-require('./startup/routes')(app);
+require("./startup/logger")(app);
+require("./startup/database")();
+require("./startup/config")(app);
+require("./startup/middleware")(app);
+require("./startup/routes")(app);
 
-app.listen(port, () => serverDebugger(`Listening on http://localhost:${port}/ ...`));
+app.listen(port, () =>
+  serverDebugger(`Listening on http://localhost:${port}/ ...`)
+);
