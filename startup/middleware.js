29706f0ad230c7eae99d1190130ab9dbd970995c
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-08-26 17:52:43 +0300

    added i18n custom middleware

--- startup/middleware.js
+++ startup/middleware.js
@@ -1,7 +1,15 @@
-const { json } = require('express');
-const helmet = require('helmet');
+const config = require("config");
+const { json } = require("express");
+const helmet = require("helmet");
 
-module.exports = function (app) {
+// custom middleware
+const i18n = require("./middleware/i18n");
+
+module.exports = function(app) {
   app.use(json());
   app.use(helmet());
+
+  if (config.get("localization.activate")) {
+    app.use(i18n);
+  }
 };
