a477a8ebb3337dbede3b568c2dab31d87da12211
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-11-07 19:05:37 +0300

    disabling etag and powered-by

--- src/startup/config.js
+++ src/startup/config.js
@@ -1,10 +1,19 @@
-const { static } = require('express');
-const config = require('config');
-const serverDebugger = require('debug')('app:server');
+const { static } = require("express");
+const config = require("config");
+const serverDebugger = require("debug")("app:server");
 
 module.exports = function(app) {
-  if (config.has('static') && config.get('static')) {
-    app.use(static(config.get('static')));
-    serverDebugger(`Serve static files is active "${config.get('static')}".`);
+  if (config.has("static") && config.get("static")) {
+    app.use(static(config.get("static")));
+    serverDebugger(`Serve static files is active "${config.get("static")}".`);
+  }
+
+  // note: if you are developing api disable that
+  if (config.has("etag") && !config.get("etag")) {
+    app.disable("etag");
+  }
+
+  if (config.has("powered-by") && config.get("powered-by")) {
+    app.disable("x-powered-by");
   }
 };
