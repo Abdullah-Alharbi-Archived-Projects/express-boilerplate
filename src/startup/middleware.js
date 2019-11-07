589850f1817794b8df21f6a29409377a9159ce78
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-11-07 16:33:31 +0300

    added compression middleware

--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -1,7 +1,9 @@
 const { json } = require("express");
 const helmet = require("helmet");
+const compression = require("compression");
 
 module.exports = function(app) {
   app.use(json());
   app.use(helmet());
+  app.use(compression()); // compress all responses
 };
