5f08c8079a39d6f0a9d639b20b711606dc6aa9af
W2AlharbiMe <w2.alharbi.me@gmail.com>
2020-03-12 23:23:53 +0300

    added method-override dependenciy and set it up in middleware.js

--- src/startup/middleware.js
+++ src/startup/middleware.js
@@ -1,9 +1,15 @@
 const { json } = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
+const methodOverride = require("method-override");
 
 module.exports = function(app) {
   app.use(json());
   app.use(helmet());
   app.use(compression()); // compress all responses
+
+  // methodOverride by header
+  app.use(methodOverride("X-HTTP-Method"));
+  // and by attach ?_method=POST etc... to override the request
+  app.use(methodOverride("_method"));
 };
