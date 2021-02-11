ac422a2963a58444c8b71faa5af098828dabd467
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-02-12 02:21:52 +0300

    server configuration system #17

---
+++ src/config/server.js
@@ -0,0 +1,67 @@
+const { env, boolOnEnv } = require('../helpers/env');
+
+module.exports = {
+
+    // static middleware configurations
+    static: {
+        // enable or disable serving static
+        enabled: env('APP_SERVER_STATIC_ENABLED', true),
+
+        folder: env('APP_SERVER_STATIC_FOLDER', 'public'),
+
+        // all static middleware configurations:
+        // http://expressjs.com/en/resources/middleware/serve-static.html
+        middleware: {
+
+            cacheControl: env('APP_SERVER_STATIC_MIDDLEWARE_CACHE_CONTROL'),
+
+            dotFiles: env('APP_SERVER_STATIC_MIDDLEWARE_DOT_FILES', false),
+
+            // enable or disable etag for static assets such as images, css etc...
+            etag: env('APP_SERVER_STATIC_MIDDLEWARE_ETAG', true),
+
+            extensions: env('APP_SERVER_STATIC_MIDDLEWARE_EXTENSIONS'),
+
+            // if there's index.html in any directory in "public" express will serve it
+            // if you need this behaviour change this to "true"
+            index: env('APP_SERVER_STATIC_MIDDLEWARE_INDEX', false),
+
+            // Enable or disable Last-Modified header, defaults to true. Uses the file system's last modified value.
+            lastModified: env('APP_SERVER_STATIC_MIDDLEWARE_LAST_MODIFIED', true),
+
+
+            immutable: env('APP_SERVER_STATIC_MIDDLEWARE_IMMUTABLE', false),
+
+            maxAge: env('APP_SERVER_STATIC_MIDDLEWARE_MAX_AGE', 0),
+
+
+            redirect: env('APP_SERVER_STATIC_MIDDLEWARE_REDIRECT', true),
+
+            // function to set custom headers for static files
+            setHeaders() { },
+        },
+
+        // static route path /public/images/first.jpg
+        route: env('APP_SERVER_STATIC_ROUTE', '/public'),
+    },
+
+
+    global: {
+        etag: env('APP_SERVER_GLOBAL_ETAG', boolOnEnv('development')),
+
+        poweredBy: env('APP_SERVER_GLOBAL_POWERED_BY', false),
+    },
+
+
+    server: {
+        
+        port: env('APP_SERVER_PORT', process.argv[2] || 8001),
+
+        hostname: env('APP_SERVER_HOST_NAME', 'localhost'),
+        
+        // if you want you can add your callback here 
+        // callback: () => {},
+    },
+
+
+};
