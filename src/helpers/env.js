f2dd9f302f5dadee1f6aad9362b3e9ac1d444361
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-02-12 02:24:11 +0300

    added a method for loading a specific config

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -1,4 +1,6 @@
-module.exports = {
+const {join} = require('path');
+
+const helpers = {
     getCurrentEnv() {
         return process.env.NODE_ENV ? process.env.NODE_ENV.toLowerCase() : 'development';
     },
@@ -7,4 +9,18 @@ module.exports = {
         return process.env[variableName.toUpperCase()] ? process.env[variableName.toUpperCase()] : defaultValue;
     },
 
+
+    loadConfig(configName = '', property = null) {
+        const config = require(`../config/${configName}.js`);
+
+        return property ? config[property] : config;
+    },
+
+    boolOnEnv(env = 'development') {
+        return helpers.getCurrentEnv() === env;
+    }
+
 };
+
+
+module.exports = helpers;
