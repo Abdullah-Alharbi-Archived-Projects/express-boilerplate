1abea0ddee98accfa8bc1b567802fede3390595d
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-02-12 03:22:11 +0300

    env helper testing #17

---
+++ src/tests/unit/env.spec.js
@@ -0,0 +1,21 @@
+describe("Environment unit tests suit", () => {
+    it("should read a property from .env file", () => {
+        require('dotenv').config();
+
+        const {env} = require('../../helpers/env');
+
+        expect(env('APP_DATABASE_NAME')).toBe('playground');
+    });
+
+    it("should return a default configurations if no environment variable.", () => {
+        const {env} = require('../../helpers/env');
+
+        expect(env('APP_TEST_NAME', 'jest test')).toBe('jest test');
+    });
+
+    it("should return null if no default value was specified.", () => {
+        const {env} = require('../../helpers/env');
+
+        expect(env('APP_TEST_NAME')).toBeNull();
+    });
+});
