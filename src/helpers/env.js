f4864af20eb10fbaa27956079cc0bef18a50eac9
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-02-12 03:07:24 +0300

    update loadConfig

--- src/helpers/env.js
+++ src/helpers/env.js
@@ -8,15 +8,18 @@ const helpers = {
     },
 
 
-    loadConfig(configName = '', property = null) {
+    loadConfig({configName = '', property = null, env = false}) {
         const config = require(`../config/${configName}.js`);
+        const prop = property ? config[property] : config;
 
-        return property ? config[property] : config;
+        if (!env) return prop;
+
+        return env == 'current' ? prop[helpers.getCurrentEnv()] : prop[env];
     },
 
     boolOnEnv(env = 'development') {
         return helpers.getCurrentEnv() === env;
-    }
+    },
 
 };
 
