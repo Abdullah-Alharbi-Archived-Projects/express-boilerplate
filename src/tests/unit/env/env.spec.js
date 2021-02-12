8cd65a67875dd313dbea4322cb7c459d369224a3
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-02-12 17:52:34 +0300

    fix paths

--- src/tests/unit/env/env.spec.js
+++ src/tests/unit/env/env.spec.js
@@ -2,19 +2,19 @@ describe("Environment unit tests suite", () => {
     it("should read a property from .env file", () => {
         require('dotenv').config();
 
-        const {env} = require('../../helpers/env');
+        const {env} = require('../../../helpers/env');
 
         expect(env('APP_DATABASE_NAME')).toBe('playground');
     });
 
     it("should return a default configurations if no environment variable.", () => {
-        const {env} = require('../../helpers/env');
+        const {env} = require('../../../helpers/env');
 
         expect(env('APP_TEST_NAME', 'jest test')).toBe('jest test');
     });
 
     it("should return null if no default value was specified.", () => {
-        const {env} = require('../../helpers/env');
+        const {env} = require('../../../helpers/env');
 
         expect(env('APP_TEST_NAME')).toBeNull();
     });
