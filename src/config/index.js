2870a2aec9fdf3e328450c7414ac60428f16558e
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-02-12 18:22:57 +0300

    basic loader #17

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    server: require('./server'),
+    views: require('./views'),
+    database: require('./database'),
+    middleware: require('./middleware'),
+    logger: require('./logger'),
+    session: require('./session'),
+};
